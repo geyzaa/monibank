5f6bca1cec444e76bef6d7d82b57991e361ce698
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-07-08 16:34:26 -0300

    vaidação cpf  primeiro digito

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,7 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g,"" );
     validaNumerosRepetidos(cpf);
-
+    validaPrimeiroDigito(cpf)
     console.log(validaNumerosRepetidos(cpf));
 }
 
@@ -21,4 +21,21 @@ function validaNumerosRepetidos(cpf) {
 
     return numerosRepetidos.includes(cpf);
 
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador; 
+        multiplicador--;
+    }
+        soma = (soma * 10) % 11;
+
+        if (soma == 10 || soma == 11) {
+            soma = 0;
+        }
+
+        return soma != cpf[9];
 }
