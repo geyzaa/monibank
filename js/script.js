6342592b0756dcec7ddbadaa01abff406a5bec2c
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-08-16 11:01:11 -0300

    mensagens customizadas

--- js/script.js
+++ js/script.js
@@ -9,6 +9,47 @@ camposDoFormulario.forEach((campo) => {
     //para tirar a mensagem automática do formulário de preenchimento e poder costumizar as mensagens
 }) 
 
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'tooShort',
+    'customError'
+]
+
+//dentro da variavel foi criado um objeto, com várias propriedades (nome dos campos)
+//e cada um dele tbm recebe um objeto(nome do erro)
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    rg: {
+        valueMissing: "O campo de RG não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um RG válido.",
+        tooShort: "O campo de RG não tem caractéres suficientes."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caractéres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
+    }
+}
+
 function verificaCampo(campo) {
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
