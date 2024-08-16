3a91283ffe5b56eb9a63d629e9308e8db784a5db
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-08-16 10:54:10 -0300

    validity state

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ const camposDoFormulario = document.querySelectorAll("[required]");
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo)); 
-}) //"blur" tira o foco do input, é o gatilho pra verificar 
+    //"blur" tira o foco do input, é o gatilho pra verificar 
+    campo.addEventListener("invalid", evento => evento.preventDefault());
+    //para tirar a mensagem automática do formulário de preenchimento e poder costumizar as mensagens
+}) 
 
 function verificaCampo(campo) {
     if (campo.name == "cpf" && campo.value.length >= 11) {
@@ -13,5 +16,6 @@ function verificaCampo(campo) {
     if (campo.name == "aniversario" && campo.value != "") {
         ehMaiorDeIdade(campo);
     }
+    console.log(campo.validity);
 }
 
